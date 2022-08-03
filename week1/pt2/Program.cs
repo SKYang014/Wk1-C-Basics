a0cd50b636772e721cad5b26267c52125952eb22
Sarah Yang <sarahyang@rocketmortgage.com>
2022-08-03 10:42:21 -0700

    added to lab 3

--- week1/pt2/Program.cs
+++ week1/pt2/Program.cs
@@ -15,20 +15,15 @@ do
         else
         {
             directions = true;
-            //take input, calculatedata...for loop amount of ints
-            //double calcData = Math.Pow(ogNum, ogNum);
-            //Console.WriteLine("Number\t\tSquared\t\tCubed\n=======\t\t=======\t\t======");
             Console.WriteLine("-------------------------------");
             Console.WriteLine("  Number|  Squared|   Cubed  ");
             Console.WriteLine("-------------------------------");
             for (int i = 0; i < ogNum; i++)
             {
             Console.WriteLine(String.Format("{0,7} | {1,7} | {2,7}", (i + 1), Math.Pow((i + 1), 2), Math.Pow((i + 1), 3)));
-            //Console.WriteLine(String.Format("{0,-10} | {1,-10} | {2,-10}", "Edna", "Parker", 114));
-            //Console.WriteLine(String.Format("{0,-10} | {1,-10} | {2,-10}", "Johnny", "Depp", 44));
+
             Console.WriteLine("-------------------------------");
                 //take calculated data and display table
-                //Console.WriteLine((i + 1) + "\t\t" + Math.Pow((i + 1), 2) + "\t\t" + Math.Pow((i + 1), 3));
             }
             Console.WriteLine("Would you like to get learnt again? (yes or no)");
             string decision = Console.ReadLine();
